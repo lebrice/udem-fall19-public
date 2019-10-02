b2eb5f8cc187340cc5231cdeff0505c475533b65
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-10-01 20:25:40 -0400

    Pure Pursuit is working!
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- notebooks/code/exercise_03_control/controller.py
+++ notebooks/code/exercise_03_control/controller.py
@@ -1,5 +1,17 @@
 import numpy as np
 
+def wrap(angle: float) -> float:
+    """Takes in any angle (in radians), and expresses it inside the [-np.pi, np.pi] range.
+    """
+    two_pi = 2 * np.pi
+    angle %= two_pi
+    if angle < 0:
+        angle += two_pi
+    # angle is now inside [0, 2pi]
+    if angle > np.pi:
+        angle -= two_pi
+    return angle
+
 
 class Controller():
     def __init__(self, k_p_dist=10.0, k_d_dist=0., k_p_angle=10.0, k_d_angle=0):
@@ -13,6 +25,10 @@ class Controller():
         self.k_p_dist = k_p_dist
         self.k_d_dist = k_d_dist
 #         print(self.k_p_dist, self.k_d_dist, self.k_p_angle, self.k_d_angle)
+        
+        self.k_v = 0.5
+        import collections
+        self.points = collections.defaultdict(list)
 
     def angle_control_commands(self, dist, angle):
         # Return the angular velocity in order to control the Duckiebot so that it follows the lane.
@@ -71,7 +87,7 @@ class Controller():
         iterations = 0
         
         lookup_distance = follow_dist
-        multipler = 0.5
+        multiplier = 0.5
         curve_point = None
         
         while iterations < 10:            
@@ -80,7 +96,12 @@ class Controller():
             #TODO 1: Modify follow_point so that it is a function of closest_point, closest_tangent, and lookup_distance
             #
             ########
-            follow_point = closest_point
+            def find_follow_point(closest_point, closest_tangent, lookup_distance):
+                follow_point = closest_point + lookup_distance * closest_tangent
+                # print("closest_point:", closest_point , "closesest_tangent", closest_tangent, "lookup_distance", lookup_distance)
+                return follow_point
+                
+            follow_point = find_follow_point(closest_point, closest_tangent, lookup_distance)
             
             curve_point, _ = closest_curve_point(follow_point, angle)
 
@@ -95,7 +116,66 @@ class Controller():
         #TODO 2: Modify omega
         #
         ########
-        omega = 0.
-        v = 0.5
+        print("curve point:", curve_point, "pos:", pos)
+        robot_to_curve_point = curve_point - pos
+        # print("local:", curve_point_local)
+        # sin_alpha = sin(theta_g - angle)
+        # sin(A - B) = sinAcosB-cosAsinB
+        # hypothenuse = np.sqrt(robot_to_curve_point[0] ** 2 + robot_to_curve_point[2] ** 2)
+        # sin_theta_g = robot_to_curve_point[0] / hypothenuse
+        # cos_theta_g = robot_to_curve_point[2] / hypothenuse
+        # sin_alpha_1 = sin_theta_g * np.cos(angle) - cos_theta_g * np.sin(angle)
+        
+        robot_to_curve_point = curve_point - pos
+        robot_to_curve_point_angle = np.arctan2(-robot_to_curve_point[2], robot_to_curve_point[0])
+        alpha = robot_to_curve_point_angle - angle
+        alpha = wrap(alpha)
+
+        sin_alpha = np.sin(alpha)
+        
+        self.points["alpha"].append(alpha)
+        self.points["sin_alpha"].append(sin_alpha)
+        # need to tune this.
+        self.k_l = 0.5
+        self.k_v = 1.0
+        
+        max_speed = 0.5
+
+        v = min(self.k_v / abs(alpha), max_speed)
+        
+        # v = abs(self.k_v / alpha)
+        lookup_d = self.k_l * v
+
+        omega = sin_alpha / lookup_d 
+
+        print("v", v)
+        print("omega", omega)
+        print("alpha:", alpha)
+
+        # cross-track error
+        cross_track_error = sin_alpha * np.linalg.norm(robot_to_curve_point)
+        self.points["cross_track_error"].append(cross_track_error)
+        # angle error,
+        angle_of_tangent = np.arctan2(-closest_tangent[2], closest_tangent[0])
+        angle_error = angle_of_tangent - angle
+        angle_error = wrap(angle_error)
+        self.points["angle_error"].append(angle_error)
+        # commanded linear velocity
+        self.points["commanded_linear_velocity"].append(v)
+        # commanded angular velocity 
+        self.points["commanded_angular_velocity"].append(omega)
+
 
         return v, omega
+    
+    def plot(self):
+        import matplotlib.pyplot as plt
+
+        for i, (title, list_of_points) in enumerate(self.points.items()):
+            data_np = np.asarray(list_of_points)
+            plt.plot(data_np)
+
+        plt.legend(self.points.keys(), loc="upper right")
+        plt.title("Fabrice Normandin")
+
+
