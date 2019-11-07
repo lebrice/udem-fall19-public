2ff8d75fe47bddcbebd6315f6770109ac2246c11
Fabrice Normandin <fabrice.normandin@gmail.com>
2019-11-06 21:57:24 -0500

    CANNY WORKS! (it seems)
    Signed-off-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- catkin_ws/src/sw06-CV/packages/sw_06_line_detector/include/sw_06_line_detector/sw_06_cv_functions.py
+++ catkin_ws/src/sw06-CV/packages/sw_06_line_detector/include/sw_06_line_detector/sw_06_cv_functions.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from numpy.lib.stride_tricks import as_strided
-
+import traceback
 # import rospy
 
 ## Software Exercise 6: Choose your category (1 or 2) and replace the cv2 code by your own!
@@ -25,27 +25,49 @@ def dilate(bitwise, kernel):
 
 ## CATEGORY 2
 def Canny(image, threshold1, threshold2, apertureSize=3):
-	print("CANNYYYYY")
+
 	if apertureSize != 3:
 		import warnings
 		warnings.warn(UserWarning("Using apertureSize of 3."))
 		apertureSize = 3
+	
 	num_channels = image.shape[-1] if len(image.shape) == 3 else 1
-	output = np.zeros_like(image)
 	# print("num channels:", num_channels)
 	# print("HERE")
-	outputs = []
-	for channel in range(num_channels):
-		smoothed_image = gaussian_blurring(image[..., channel], std=1, kernel_size=5)
-		dx, dy = image_derivatives(smoothed_image)
-		edge_gradients = np.sqrt(dx ** 2 + dy ** 2)
+	try:
+		outputs = []
+		edge_gradients 	= np.zeros_like(image)
+		dxs 			= np.zeros_like(image)
+		dys 			= np.zeros_like(image)
+		for channel in range(num_channels):
+			smoothed_image = gaussian_blurring(image[..., channel], std=1, kernel_size=5)
+			dx, dy = image_derivatives(smoothed_image)
+			dxs[..., channel] = dx
+			dys[..., channel] = dy
+		
+		edge_gradients = np.sqrt(dxs ** 2 + dys ** 2)
+		
+		edge_gradients = np.mean(edge_gradients, axis=-1)
+		dx = np.mean(dxs, axis=-1)
+		dy = np.mean(dys, axis=-1)
+		
+		# TODO: use the grad magnitude and dx and dy's from the channel with the highest gradient.
+		# max_grad_indices = np.argmax(edge_gradients, axis=-1)
+		# edge_gradients = edge_gradients[max_grad_indices]
+		# dx = dxs[max_grad_indices]
+		# dy = dys[max_grad_indices]
+		
 		gradient_directions = snap_angles(np.arctan2(dy, dx))
-		masked_image = non_maximum_suppression(smoothed_image, gradient_directions)
-		output = hysteresis_thresholding(masked_image, edge_gradients, threshold1, threshold2)
-		outputs.append(output)
-	return np.stack(outputs, axis=-1)
 
-	# return cv2.Canny(image, threshold1, threshold2, apertureSize=3)
+		masked_image = non_maximum_suppression(edge_gradients, gradient_directions)
+		result = hysteresis_thresholding(masked_image, edge_gradients, threshold1, threshold2)
+		# expected = cv2.Canny(image, threshold1, threshold2, apertureSize=3)
+		# print(result.shape, expected.shape)
+		return result
+	except Exception as e:
+		traceback.print_exc()
+
+	
 
 
 def hysteresis_thresholding(image, image_gradients, min_val, max_val):
@@ -69,12 +91,12 @@ def hysteresis_thresholding(image, image_gradients, min_val, max_val):
 		max_x = min(i+1, image_w)
 		min_y = max(j-1, 0)
 		max_y = min(j+1, image_h)
-		indices = [
+		indices = set(
 			(x, y)
 			for x in range(min_x, max_x)
 			for y in range(min_y, max_y)
-		]
-		indices.remove((i, j))
+		)
+		indices.discard((i, j))
 		return indices
 		# return np.array(indices)
 	
@@ -85,7 +107,7 @@ def hysteresis_thresholding(image, image_gradients, min_val, max_val):
 	while unexplored:
 		index = unexplored.pop()
 		# print(index)
-		neighbours = set(neighbourhood(index))
+		neighbours = neighbourhood(index)
 		# print(neighbours)
 		# print("strong indices", strong_indices_set)
 		# print("weak indices", weak_indices_set)
@@ -107,7 +129,7 @@ def hysteresis_thresholding(image, image_gradients, min_val, max_val):
 	out[~mask] = 0.
 	return out
 
-def non_maximum_suppression(image, gradient_directions, out=None):
+def non_maximum_suppression(image_gradients, gradient_directions):
 	"""Non-maximum suppression
 	
 	To be honest, I'm very proud of this piece of code. No for-loops were needed.
@@ -115,32 +137,29 @@ def non_maximum_suppression(image, gradient_directions, out=None):
 	Arguments:
 		image {[type]} -- the image to preform non-maximum suppresion on.
 		gradient_directions {[type]} -- the gradient directions
-	"""
-	if out is None:
-		out = np.copy(image)
-	
+	"""	
 	# Get where to check depending on the "direction"
 	direction_offset_x = np.round(np.cos(gradient_directions)).astype(int)
 	direction_offset_y = np.round(np.sin(gradient_directions)).astype(int)
 	direction_offset = np.dstack((direction_offset_x, direction_offset_y))
 		
 	# the (i, j) indices of all points in the image.
-	row, col = np.indices(image.shape)
+	row, col = np.indices(image_gradients.shape)
 	# in order not to cause any indexing errors, we create a
 	# padded version of the image with the edge values duplicated.
 	# a pixel at (row, col) in the image is located at (row+1, col+1) in the padded image.
-	image_ = np.pad(image, 1, mode="edge")
+	image_ = np.pad(image_gradients, 1, mode="edge")
 	row_, col_ = row + 1, col + 1
 	# get the image pixels before and after each pixel in the image. 
-	pixel_middle = image[row, col]
+	pixel_middle = image_gradients[row, col]
 	pixel_forward  = image_[row_ + direction_offset_x, col_ + direction_offset_y]
 	pixel_backward = image_[row_ - direction_offset_x, col_ - direction_offset_y]
 
 	higher_than_forward  = pixel_middle > pixel_forward
 	higher_than_backward = pixel_middle > pixel_backward
 	is_local_maximum = higher_than_backward & higher_than_forward
+	out = np.copy(image_gradients)
 	out[~is_local_maximum] = 0
-
 	return out
 			
 
@@ -274,16 +293,16 @@ def gaussian_derivative_1d(sigma, kernel_size):
 def HoughLinesP(image, rho, theta, threshold, lines, minLineLength, maxLineGap):
 	return cv2.HoughLinesP(image, rho, theta, threshold, lines, minLineLength, maxLineGap)
 
-X = np.random.rand(80, 160, 3)
-print(X.shape)
-# X = np.array([
-# 	[1, 1, 2, 4, 5],
-# 	[5, 6, 12, 8, 6],
-# 	[3, 2, 15, 123, 7],
-# 	[5, 1, 23, 3, 6],
-# 	[1, 2, 3, 4, 8],
-# ])
+# X = np.random.rand(80, 160, 3)
+# print(X.shape)
+# # X = np.array([
+# # 	[1, 1, 2, 4, 5],
+# # 	[5, 6, 12, 8, 6],
+# # 	[3, 2, 15, 123, 7],
+# # 	[5, 1, 23, 3, 6],
+# # 	[1, 2, 3, 4, 8],
+# # ])
 
-# X = np.tile(X[..., np.newaxis], (1, 1, 3))
-bob = Canny(X, 0.1, 0.9)
-print(bob.shape)
+# # X = np.tile(X[..., np.newaxis], (1, 1, 3))
+# bob = Canny(X, 0.1, 25)
+# print(bob.shape)
