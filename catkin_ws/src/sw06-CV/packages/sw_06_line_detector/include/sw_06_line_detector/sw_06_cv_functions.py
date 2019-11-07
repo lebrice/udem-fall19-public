99258b67ff8c22ec35f1be058cf3286be8d5e993
Fabrice <fabrice.normandin@gmail.com>
2019-11-07 18:25:19 -0500

    Image derivatives are too small!
    Signed-off-by: Fabrice <fabrice.normandin@gmail.com>

--- catkin_ws/src/sw06-CV/packages/sw_06_line_detector/include/sw_06_line_detector/sw_06_cv_functions.py
+++ catkin_ws/src/sw06-CV/packages/sw_06_line_detector/include/sw_06_line_detector/sw_06_cv_functions.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 from numpy.lib.stride_tricks import as_strided
 import traceback
+import warnings
+import numpy
+
 # import rospy
 
 ## Software Exercise 6: Choose your category (1 or 2) and replace the cv2 code by your own!
@@ -25,7 +28,6 @@ def dilate(bitwise, kernel):
 ## CATEGORY 2
 def Canny(image, threshold1, threshold2, apertureSize=3):
 	if apertureSize != 3:
-		import warnings
 		warnings.warn(UserWarning("Using apertureSize of 3, even though a different value was passed."))
 		apertureSize = 3	
 	try:
@@ -63,6 +65,11 @@ def get_image_gradients(image):
 		dxs[..., channel] = dx
 		dys[..., channel] = dy
 	
+	## TODO: comment this out, just testing to check if the sobel operation is the issue
+	# sobel_x = cv2.Sobel(image, cv2.CV_8U, 1, 0, scale=1, ksize=3)
+	# sobel_y = cv2.Sobel(image, cv2.CV_8U, 0, 1, scale=1, ksize=3)
+	# dxs, dys = sobel_x, sobel_y
+
 	edge_gradients = np.sqrt(dxs ** 2 + dys ** 2)
 	
 	# We use the grad magnitude and dx and dy's from the channel with the highest gradient.
@@ -70,127 +77,198 @@ def get_image_gradients(image):
 	edge_gradients = np.max(edge_gradients, axis=-1)
 	mask = np.zeros_like(image, dtype=bool)
 	mask[max_grad_indices] = True
-	dxs = dxs & mask
+	dxs &= mask
 	dx = np.sum(dxs, axis=-1)
-	dys = dys & mask
+	dys &= mask
 	dy = np.sum(dys, axis=-1)
 	return dx, dy, edge_gradients
 
+
 def hysteresis_thresholding(image, image_gradients, min_val, max_val):
+	"""Perform hysteresis thresholding using some bitwise magic.
+	
+	Arguments:
+		image {np.ndarray} -- The image. Assumed to be 2-channel.
+		image_gradients {[type]} -- [description]
+		min_val {[type]} -- [description]
+		max_val {[type]} -- [description]
+	
+	Returns:
+		[type] -- [description]
+	"""
 	print("BEFORE HYSTERISIS THRESHOLDING:", image)
 	print("gradients:", image_gradients)
-	strong_indices  = np.where(image_gradients >= max_val)
-	off_indices 	= np.where(image_gradients < min_val)
-	weak_indices 	= np.where((min_val <= image_gradients) & (image_gradients < max_val))
+	largest_gradient_value = np.max(image_gradients)
+	while largest_gradient_value < max_val:
+		print("Largest gradient value:", largest_gradient_value)
+		warnings.warn(UserWarning("Image has no edge gradients above upper threshold, increasing all gradients values!"))
+		image_gradients *= 1.5
+		largest_gradient_value = np.max(image_gradients)
 	
+	# print("Largest gradient value:", largest_gradient_value)
 	# the set of all 'strong' indices.
-	# strong_indices = np.dstack(strong_indices)
-	image_h = image.shape[0]
-	image_w = image.shape[1]
+	strong_indices  = indices_where(image_gradients >= max_val)
+	off_indices 	= indices_where(image_gradients < min_val)
+	weak_indices 	= indices_where((min_val <= image_gradients) & (image_gradients < max_val))
 	
-	def neighbourhood(index):
-		"""Returns the coordinates of the neighbours of a given coordinate or list of coordinates.
-		
-		Arguments:
-			index {np.ndarray} -- either a list of coordinates (as an ndarray) or a coordinate itself, in the form (i, j)
+	image_height = image.shape[0]
+	image_width = image.shape[1]
 
-			NOTE: the pixels neighbours are clipped of (image_height-1, )
-		
-		Returns:
-			[type] -- [description]
-		"""
+	# get the neighbours of all strong edges.
+	# convert their neighbours with weak edges to strong edges.
+	to_explore = np.zeros_like(image_gradients, dtype=bool)
+	to_explore[index_with(strong_indices)] = True
 
-		neighbourhood = np.concatenate(np.dstack((np.indices([3,3]) - 1)))
-		if len(index.shape) == 2:
-			neighbourhood = neighbourhood[:, np.newaxis, :]
-		neighbours_and_itself = index + neighbourhood
-		keep = np.ones(9, dtype=bool)
-		keep[4] = False # drop the point itself, but keep the neighbours.
-		neighbours = neighbours_and_itself[keep]
-		if len(index.shape) == 2:
-			neighbours = np.stack(neighbours, axis=1)
-		
-		mask = np.ones_like(neighbours, dtype=bool)
-		# remove all neighbours that have either a negative value in them
-		negative = np.where(neighbours < 0)
-		mask[negative] = False
-		# or a value equal to image_height in x
-		greater_than_image_height = np.where(neighbours[..., 0] >= image_h)
-		mask[greater_than_image_height] = False
-		# or image_width in z
-		greater_than_image_width = np.where(neighbours[..., 1] >= image_w)
-		mask[greater_than_image_width] = False
-		# or that correspond to an index in 'index'
-		equal_to_index = np.equal(neighbours, np.tile(index, [neighbours.shape[1]//index.shape[0], 1]))
-		equal_to_index = np.all(equal_to_index, axis=-1)
-		mask[equal_to_index] = False
-		
-		mask = np.all(mask, axis=-1)
+	explored = np.zeros_like(image_gradients, dtype=bool)
 
-		# print(mask)
-		# for i, (m, n) in enumerate(zip(mask, neighbours)):
-		# 	if len(index.shape) == 2:
-		# 		for keep, (i, j) in zip(m, n):
-		# 			print("point", i, j, "is good:", keep)
-		# 	else:
-		# 		keep = m
-		# 		i, j = n
-		# 		print("point", i, j, "is good:", keep)
-			
-		neighbours = neighbours[mask]
-		return neighbours
-		# # print(image[row, col])
-		# min_x = max(i-1, 0)
-		# max_x = min(i+1, image_w-1)
-		# min_y = max(j-1, 0)
-		# max_y = min(j+1, image_h-1)
-		# indices = set(
-		# 	(x, y)
-		# 	for x in range(min_x, max_x + 1)
-		# 	for y in range(min_y, max_y + 1)
-		# )
+	strong = np.zeros_like(image_gradients, dtype=bool)
+	strong[index_with(strong_indices)] = True
+	# print("strong:", strong)
+
+	weak = np.zeros_like(image_gradients, dtype=bool)
+	weak[index_with(weak_indices)] = True
+
+	unexplored_indices = aggregate(np.nonzero(to_explore))
+	# print("unexplored (initial):", [str(v) for v in unexplored])
+	# print("weak indices (initial):", [str(v) for v in weak_indices])
+	# print("off indices (initial):", [str(v) for v in off_indices])
+	already_explored = np.zeros_like(to_explore)
+
+	while len(unexplored_indices) > 0:
+		
+		# print("exploring indices ", [str(v) for v in indices])
 		# print(indices)
-		# indices.discard((i, j))
-		# return indices
-		# # return np.array(indices)
-	# print(neighbourhood(np.array([4, 4])))
-	# exit()	
-	# print(neighbourhood(np.array([[1, 1], [2,2]])))
-	# exit()
 
+		neighbours = neighbourhood(unexplored_indices, image_width, image_height)
+		is_neighbour = np.zeros_like(weak)
+		is_neighbour[index_with(neighbours)] = True
+		is_weak_neighbour = is_neighbour & weak
+		weak_neighbours = aggregate(np.nonzero(is_weak_neighbour))
+		# weak_neighbours = common_rows_between(neighbours, weak_indices)
 
-	strong_indices_set = set(zip(*strong_indices))
-	weak_indices_set = set(zip(*weak_indices))
+		# print("The neighbours of (", ",".join(str(pixel) for pixel in indices), ") are ", neighbours)
+		# print("weak neighbours:", [str(v) for v in weak_neighbours])
+		
+		strong[index_with(weak_neighbours)] = True
+		weak[index_with(weak_neighbours)] = False
+		# mark that we need to explore these:
+		
+		already_explored[index_with(unexplored_indices)] = True
+		# explore the indices of the weak neighbours, if they haven't been explored already.
+		to_explore[index_with(weak_neighbours)] = True
+		# do not re-explore already explored indices.
+		to_explore &= ~already_explored
+		
+		unexplored_indices = aggregate(np.nonzero(to_explore))
 	
-	unexplored = strong_indices_set.copy()
-	unexplored = np.copy(strong_indices)
-	while len(unexplored) > 0:
-		# index = unexplored.pop()
-		indices = unexplored
-		# print(index)
-		neighbours = neighbourhood(indices)
-		print("The neighbours of ", indices, " are ", neighbours)
-		# print(neighbours)
-		# print("strong indices", strong_indices_set)
-		# print("weak indices", weak_indices_set)
-		np.setdiff1d()
-		weak_neighbours = np.isin(neighbours, weak_indices_set)
-		# print("weak neighbours:", weak_neighbours)
-		# explore the weak neighbours.
-		unexplored.update(weak_neighbours)
-		# store the (newly) strong neighbours.
-		strong_indices_set.update(weak_neighbours)
-		weak_indices_set.difference_update(weak_neighbours)
-	
-	kept_indices = (
-		np.array([x for (x, y) in strong_indices_set], dtype=int),
-		np.array([y for (x, y) in strong_indices_set], dtype=int)
-	)
-	
-	out = np.zeros_like(image)
-	out[kept_indices] = 255
+	out = np.zeros_like(image_gradients)
+	out[~strong] = 0
+	out[strong] = 255
 	print("AFTER HYSTERISIS THRESHOLDING:", out)
 	return out
+
+
+def aggregate(list_of_indices):
+	return np.concatenate(np.dstack(list_of_indices))
+
+def indices_where(condition):
+	return np.concatenate(np.dstack(np.where(condition)))
+
+def index_with(list_of_indices):
+	return list_of_indices[:, 0], list_of_indices[:, 1]
+
+def neighbourhood(index, image_width, image_height):
+	"""Returns the coordinates of the neighbours of a given coordinate or list of coordinates.
+	
+	Arguments:
+		index {np.ndarray} -- either a list of coordinates (as an ndarray) or a coordinate itself, in the form (i, j)
+
+		NOTE: the pixels neighbours are clipped of (image_height-1, )
+	
+	Returns:
+		np.ndarray -- ndarray of shape [?, 2], which contains the indices of the neighbouring pixels
+	"""
+	neighbourhoods = np.concatenate(np.dstack((np.indices([3,3]) - 1)))
+	if len(index.shape) == 2:
+		neighbourhoods = neighbourhoods[:, np.newaxis, :]
+
+	neighbours_and_itself = index + neighbourhoods
+	keep = np.ones(9, dtype=bool)
+	keep[4] = False # drop the point itself, but keep the neighbours.
+	neighbours = neighbours_and_itself[keep]
+	if len(index.shape) == 2:
+		neighbours = np.stack(neighbours, axis=1)
+	
+	mask = np.ones_like(neighbours, dtype=bool)
+	# remove all neighbours that have either a negative value in them
+	negative = np.where(neighbours < 0)
+	mask[negative] = False
+	# or a value equal to image_height in x
+	greater_than_image_height = np.where(neighbours[..., 0] >= image_height)
+	mask[greater_than_image_height] = False
+	# or image_width in z
+	greater_than_image_width = np.where(neighbours[..., 1] >= image_height)
+	mask[greater_than_image_width] = False
+	# or that correspond to an index in 'index'
+	tiled = np.expand_dims(index, 1)
+	tiled = np.tile(tiled, (1, neighbours.shape[1], 1))
+	equal_to_index = np.equal(neighbours, tiled)
+	equal_to_index = np.all(equal_to_index, axis=-1)
+	mask[equal_to_index] = False
+	
+	mask = np.all(mask, axis=-1)
+
+	# print(mask)
+	# for i, (m, n) in enumerate(zip(mask, neighbours)):
+	# 	if len(index.shape) == 2:
+	# 		for keep, (i, j) in zip(m, n):
+	# 			print("point", i, j, "is good:", keep)
+	# 	else:
+	# 		keep = m
+	# 		i, j = n
+	# 		print("point", i, j, "is good:", keep)
+		
+	neighbours = neighbours[mask]
+	# get rid of duplicates:
+	neighbours = np.unique(neighbours, axis=0)
+	return neighbours
+	# # print(image[row, col])
+	# min_x = max(i-1, 0)
+	# max_x = min(i+1, image_w-1)
+	# min_y = max(j-1, 0)
+	# max_y = min(j+1, image_h-1)
+	# indices = set(
+	# 	(x, y)
+	# 	for x in range(min_x, max_x + 1)
+	# 	for y in range(min_y, max_y + 1)
+	# )
+	# print(indices)
+	# indices.discard((i, j))
+	# return indices
+	# # return np.array(indices)
+
+
+def common_rows_between(array_1, array_2):
+	"""TAKEN FROM https://stackoverflow.com/questions/8317022/get-intersecting-rows-across-two-2d-numpy-arrays
+	
+	Arguments:
+		array_1 {np.ndarray} -- a 2d array
+		array_2 {np.ndarray} -- another 2d array
+	
+	Returns:
+		np.ndarray -- a 2d array containing the common rows in both array_1 and array_2. 
+	"""
+	nrows, ncols = array_1.shape
+	dtype={
+		'names': ['f{}'.format(i) for i in range(ncols)],
+		'formats': ncols * [array_1.dtype]
+	}
+	C = np.intersect1d(array_1.view(dtype), array_2.view(dtype))
+	# This last bit is optional if you're okay with "C" being a structured array...
+	C = C.view(array_1.dtype).reshape(-1, ncols)
+	return C
+
+
 
 def non_maximum_suppression(image, image_gradients, gradient_directions):
 	"""Non-maximum suppression
@@ -249,6 +327,7 @@ def image_derivatives(image):
 	Returns:
 		[type] -- [description]
 	"""
+	
 	sobel_sign = np.array([[-1, 0, 1]])
 	sobel_mag = np.array([[1, 2, 1]])
 
@@ -257,7 +336,21 @@ def image_derivatives(image):
 
 	temp = conv2d(image, sobel_mag)
 	image_dy = conv2d(temp, sobel_sign.T)
+	
+	return image_dx, image_dy
 
+	# Slower alternative (from OpenCV docs):
+	sobel_x = np.array([
+		[-1, 0, 1],
+		[-2, 0, 2],
+		[-1, 0, 1],
+	])
+	image_dx_1, image_dy_1 = image_dx, image_dy
+
+	image_dx = conv2d(image, sobel_x)
+	image_dy = conv2d(image, sobel_x.T)
+	assert np.all(np.isclose(image_dy, image_dy_1))
+	assert np.all(np.isclose(image_dx, image_dx_1))
 	return image_dx, image_dy
 
 def conv2d(x, kernel, stride=1, padding="auto", padding_mode="edge"):
@@ -301,13 +394,13 @@ def conv2d(x, kernel, stride=1, padding="auto", padding_mode="edge"):
 	return np.dot(flattened_patches, flattened_kernel).reshape(output_shape)
 
 
-def separable_conv2d(x, kernel_1d, stride=1, padding="auto"):
+def separable_conv2d(x, kernel_1d, stride=1, padding="auto", padding_mode="edge"):
 	assert len(kernel_1d.shape) == 1, kernel_1d
 	k = kernel_1d.shape[0]
 	k1 = kernel_1d.reshape([1, k])
-	result_1 = conv2d(x, k1, stride, padding)
+	result_1 = conv2d(x, k1, stride, padding, padding_mode)
 	k2 = k1.T
-	result_2 = conv2d(result_1, k2, stride, padding)
+	result_2 = conv2d(result_1, k2, stride, padding, padding_mode)
 	return result_2
 	
 
@@ -315,19 +408,38 @@ def gaussian_kernel_1d(std=1, kernel_size=5):
 	x = np.arange(-(kernel_size//2), (kernel_size//2)+1, dtype=float)
 	g = np.exp(- (x**2 / (2 * std**2))) / (np.sqrt(2 * np.pi) * std)
 	# normalize the sum to 1
-	g /= np.sum(g)
+	g = g / np.sum(g)
 	return g
 
 
 def gaussian_blurring(image, std=1, kernel_size=5):
+	# # print(kernel)
+	# kernel = np.expand_dims(kernel, axis=0)
+	# kernel = kernel.T @ kernel
+	# kernel /= np.sum(kernel)
+	# print("BEFORE GAUSSIAN BLURRING:\n", image)
+
 	kernel = gaussian_kernel_1d(std, kernel_size)
-	return separable_conv2d(image, kernel)
+	image1 = separable_conv2d(image, kernel, padding_mode="constant")
+	# print("AFTER GAUSSIAN BLURRING1:\n", image1)
+	
+	return image1
+	# slower alternative from openCV documentation:
+	kernel = 1 / 159 * np.array([
+		[2, 4, 5, 4, 2],
+		[4, 9, 12, 9, 4],
+		[5, 12, 15, 12, 5],
+		[4, 9, 12, 9, 4],
+		[2, 4, 5, 4, 2]
+	])
+	image2 = conv2d(image, kernel)
+	print("AFTER GAUSSIAN BLURRING2:\n", image2)
+	return image2
 
 
 def gaussian_derivative_filtering(image, std=1, kernel_size=5):
 	kernel = gaussian_derivative_1d(std, kernel_size)
 	return separable_conv2d(image, kernel)
-
 
 def gaussian_derivative_1d(sigma, kernel_size):
 	"""
@@ -367,9 +479,10 @@ X = np.array([
 	[5, 255, 23, 3, 6],
 	[1, 2, 3, 4, 8],
 ])
-X = np.tile(X[..., np.newaxis], (1, 1, 3))
+X = np.tile(X[..., np.newaxis], (10, 10, 3))
 X[:,:,1] = X[:,:,0] * 0.5
 X[:,:,2] = X[:,:,0] * 0.3
+
 print(X.shape)
-bob = Canny(X.astype(np.uint8), 5, 15)
+bob = Canny(X.astype(np.uint8), 20, 200)
 print(bob)
